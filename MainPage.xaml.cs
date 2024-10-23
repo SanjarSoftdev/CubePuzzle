84c77969ec3ea5ff3467f7fa2861aff10e990ba8
Sanjar <sanjar.softdev@gmail.com>
2024-10-23 14:32:16 +0500

    applying js code in MAUI

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CubePuzzle
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CubePuzzle
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
